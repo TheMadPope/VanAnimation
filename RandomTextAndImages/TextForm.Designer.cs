7fb039c7cba00288380e9203d521df4b122c170c
Richard E. Moore <nano.moore@gmail.com>
2016-10-14 19:19:12 -0700

    Add project files.

---
+++ RandomTextAndImages/TextForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RandomTextAndImages
+{
+    partial class TextForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(708, 887);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Wingdings", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(433, 519);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox2.Size = new System.Drawing.Size(254, 216);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(48, 301);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox3.Size = new System.Drawing.Size(77, 13);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(733, 912);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Analysis in progress...";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ RandomTextAndImages/TextForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Timers;
+
+namespace RandomTextAndImages
+{
+    public partial class TextForm : Form
+    {
+        public TextForm()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            var i = new ImageForm();
+            i.Show();
+            
+        }
+
+        public async void jitterText()
+        {
+            var r = new Random();
+            var iRandom = 0;
+            string text = System.IO.File.ReadAllText(@"C:\VanCode\textfile.txt");
+            var iCurrent = 0;
+            var miniCounter = 0;
+            string sup;
+            while (iCurrent < text.Length)
+            {
+                iRandom = r.Next(1, 5);
+                if((iCurrent + iRandom) > text.Length)
+                {
+                    iCurrent = 0;
+                }
+                var s = text.Substring(iCurrent, iRandom);
+                textBox1.AppendText(s);
+                if (miniCounter >= 60)
+                {
+                    textBox2.Text = null;
+                    miniCounter = 0;
+                }
+                sup = s.ToUpper();
+                textBox3.Text = string.Format("{0}:{1} -- {2}", sup, sup, sup);
+                textBox2.AppendText(s);
+                iCurrent = iCurrent + iRandom;
+                miniCounter++;
+                await Task.Delay(1);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            jitterText();
+        }
+        
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
